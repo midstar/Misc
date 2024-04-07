33c5a4cf162d01b2b8b95ec1e69a1fc242d4fa47
Joel Midstjärna <joel.midstjarna@gmail.com>
2024-04-07 22:51:45 +0200

    Using responsive layout instead of absolute

--- photocopy/photocopy_gui.py
+++ photocopy/photocopy_gui.py
@@ -105,61 +105,98 @@ def thrd_copy():
 root.geometry("1400x700")
 root.title("Photo Copy")
 
+# Top bar
+frm_top = Frame(root)
+frm_top.pack(fill=X)
+
+# Source and Dest
+frm_src_dst = Frame(frm_top)
+frm_src_dst.pack(side=LEFT, fill=X, expand=True)
+
 # Source
-lbl_src = Label(root, text ="Source") 
-lbl_src.place(x=5,y=6)
-entry_src = Entry(root,textvariable = str_src, font=('calibre',10,'normal'), width = 90)
-entry_src.place(x=65,y=6)
-btn_select_src = Button(root, text ="Select", command = cb_select_src)
-btn_select_src.place(x=975,y=2)
+frm_src = Frame(frm_src_dst)
+frm_src.pack(fill=X)
+
+lbl_src = Label(frm_src, text ="Source", width = 6) 
+lbl_src.pack(side=LEFT)
+entry_src = Entry(frm_src,textvariable = str_src, font=('calibre',10,'normal'))
+entry_src.pack(side=LEFT, fill=X, expand=True)
+btn_select_src = Button(frm_src, text ="Select", command = cb_select_src)
+btn_select_src.pack(side=RIGHT, padx=2)
 
 # Destination
-lbl_dst = Label(root, text ="Dest") 
-lbl_dst.place(x=5,y=46)
-entry_dst = Entry(root,textvariable = str_dst, font=('calibre',10,'normal'), width = 90)
-entry_dst.place(x=65,y=46)
-btn_select_dst = Button(root, text ="Select", command = cb_select_dst)
-btn_select_dst.place(x=975,y=42)
+frm_dst = Frame(frm_src_dst)
+frm_dst.pack(fill=X)
+
+lbl_dst = Label(frm_dst, text ="Dest", width = 6) 
+lbl_dst.pack(side=LEFT)
+entry_dst = Entry(frm_dst,textvariable = str_dst, font=('calibre',10,'normal'))
+entry_dst.pack(side=LEFT, fill=X, expand=True)
+btn_select_dst = Button(frm_dst, text ="Select", command = cb_select_dst)
+btn_select_dst.pack(side=LEFT, padx=2)
 
 # Run / stop button
-fnt_large = font.Font(family='Helvetica', size=26, weight=font.BOLD)
-btn_run_stop = Button(root, text ="Run", command = cb_run_stop, font=fnt_large, width=5)
-btn_run_stop.place(x=1070,y=10)
+fnt_large = font.Font(family='Helvetica', size=20, weight=font.BOLD)
+btn_run_stop = Button(frm_top, text ="Run", command = cb_run_stop, font=fnt_large, width=5)
+btn_run_stop.pack(side=LEFT, padx=5)
+
 
 # Stats
-fnt_small = font.Font(family='Helvetica', size=8)
+frm_stats = Frame(frm_top)
+frm_stats.pack(side=RIGHT)
 
-lbl_total = Label(root, text ="Total:", font=fnt_small) 
-lbl_total.place(x=1260,y=2)
-lbl_total_val = Label(root, text ="0", font=fnt_small) 
-lbl_total_val.place(x=1310,y=2)
+frm_stats1 = Frame(frm_stats)
+frm_stats1.pack(fill=X)
 
-lbl_left = Label(root, text ="Left:", font=fnt_small) 
-lbl_left.place(x=1260,y=18)
-lbl_left_val = Label(root, text ="0", font=fnt_small) 
-lbl_left_val.place(x=1310,y=18)
+frm_stats2 = Frame(frm_stats)
+frm_stats2.pack(fill=X)
 
-lbl_copied = Label(root, text ="Copied:", font=fnt_small) 
-lbl_copied.place(x=1260,y=34)
-lbl_copied_val = Label(root, text ="0", font=fnt_small) 
-lbl_copied_val.place(x=1310,y=34)
+frm_stats3 = Frame(frm_stats)
+frm_stats3.pack(fill=X)
 
-lbl_existed = Label(root, text ="Existed:", font=fnt_small) 
-lbl_existed.place(x=1260,y=50)
-lbl_existed_val = Label(root, text ="0", font=fnt_small) 
-lbl_existed_val.place(x=1310,y=50)
+fnt_small = font.Font(family='Helvetica', size=7)
 
-lbl_failed = Label(root, text ="Failed:", font=fnt_small) 
-lbl_failed.place(x=1260,y=66)
-lbl_failed_val = Label(root, text ="0", font=fnt_small) 
-lbl_failed_val.place(x=1310,y=66)
+frm_total = Frame(frm_stats1)
+frm_total.pack(side=LEFT)
+lbl_total = Label(frm_total, text ="Total:", font=fnt_small, width = 7) 
+lbl_total.pack(side=LEFT)
+lbl_total_val = Label(frm_total, text ="0", font=fnt_small, width = 5) 
+lbl_total_val.pack(side=RIGHT)
+
+frm_left = Frame(frm_stats1)
+frm_left.pack(side=RIGHT)
+lbl_left = Label(frm_left, text ="Left:", font=fnt_small, width = 7) 
+lbl_left.pack(side=LEFT)
+lbl_left_val = Label(frm_left, text ="0", font=fnt_small, width = 5) 
+lbl_left_val.pack(side=RIGHT)
+
+frm_copied = Frame(frm_stats2)
+frm_copied.pack(side=LEFT)
+lbl_copied = Label(frm_copied, text ="Copied:", font=fnt_small, width = 7)  
+lbl_copied.pack(side=LEFT)
+lbl_copied_val = Label(frm_copied, text ="0", font=fnt_small, width = 5)  
+lbl_copied_val.pack(side=RIGHT)
+
+frm_existed = Frame(frm_stats2)
+frm_existed.pack(side=RIGHT)
+lbl_existed = Label(frm_existed, text ="Existed:", font=fnt_small, width = 7)
+lbl_existed.pack(side=LEFT)
+lbl_existed_val = Label(frm_existed, text ="0", font=fnt_small, width = 5)
+lbl_existed_val.pack(side=RIGHT)
+
+frm_failed = Frame(frm_stats3)
+frm_failed.pack(side=LEFT)
+lbl_failed = Label(frm_failed, text ="Failed:", font=fnt_small, width = 7)
+lbl_failed.pack(side=LEFT)
+lbl_failed_val = Label(frm_failed, text ="0", font=fnt_small, width = 5)
+lbl_failed_val.pack(side=RIGHT)
 
 # Progress bar
 progressbar = ttk.Progressbar(variable = progress)
-progressbar.place(x=5, y=90, width=1390)
+progressbar.pack(fill=X, padx = 5, pady = 5)
 
 # Status text entry
-scr_text = ScrolledText(root, width=137, height=25)
-scr_text.place(x=5, y=110)
+scr_text = ScrolledText(root)
+scr_text.pack(fill=BOTH, expand = True)
 
 root.mainloop()     
